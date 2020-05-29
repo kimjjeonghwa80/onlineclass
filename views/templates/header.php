c0a2cb4b8c673e68a13b430329771f09b4bca804
root <info@keer.biz>
2020-05-29 11:37:33 +0100

    add navbar style.css

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,1 +1,14 @@
-<header>HEADER ICI</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Online Class</title>
+</head>
+<div class="navbar">
+    <a href="#">HOME</a>
+    <a href="#">ARTICLES</a>
+    <a href="#">PORTFOLIO</a>
+    <a href="#">ABOUT</a>
+    <a href="#">CONTACT</a>
+</div>
