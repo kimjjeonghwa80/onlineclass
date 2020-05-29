0e15651143c00047cddd1970ae9d6db4ea0aac4a
root <info@keer.biz>
2020-05-29 20:21:41 +0100

    .htaccess fix

--- router/Router.php
+++ router/Router.php
@@ -15,39 +15,49 @@ class Router {
         $this->controller_list = ['index', 'login'];
         $this->controller_name = false;
         $this->controller = false;
-        $this->root = $this->parseRoot($self);
+        //$this->root = $this->parseRoot($self);
+        $this->root = "/";
         $this->route = $this->parseURL($url);
         $this->run();
 
-        var_dump("self : ", $self);
-        var_dump("url : ", $url);
+        //var_dump("self : ", $self);
+        //var_dump("url : ", $url);
+        //var_dump($_SERVER['PHP_SELF']);                     //   /login.php
     }
     
     private function parseRoot($self) {
-        return str_replace('index.php', '', $self);
+        //return str_replace('index.php', '', $self);
+        return "/";
     }
     
     private function parseURL($url) {
-        //var_dump("url : ", $url);                    //         $url = /login.php
-        //var_dump("root : ", $this->root);          //         $this->root = /
-        $path = str_replace($this->root, '', $url);  //         $path = "" (login.php)
-        //var_dump($path);
+        //var_dump("url : ", $url);                         //         $url = /login.php
+        //var_dump("root : ", $this->root);                 //         $this->root = /
+        $path = str_replace($this->root, '', $url);         //         $path = "" (login.php)
+        //$path = $url;
+        
 
         $path = explode('/', $path);
-
+        
         //var_dump($path);
         
         if($path && $path[0]) {
             $path[0]=substr($path[0],0,strpos($path[0],"?"));
+            
         }
+        
+        
         
         $controller = false;
         if($path && count($path) && strlen($path[0])) {
             $controller = $path[0];
+            //
+            
         } else if(count($path) && !strlen($path[0])) {
             $controller = 'index';
+            //var_dump("TRUE");
         }
-        
+        var_dump($controller);
         if($controller && in_array($controller, $this->controller_list)) {
             $this->controller_name = ucfirst($controller.'Controller');
         } else {
